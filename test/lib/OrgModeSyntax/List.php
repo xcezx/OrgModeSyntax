64b3ac4b7f399e391a47bf926265de0997a2a4b2
xcezx <main.xcezx@gmail.com>
2010-07-16 03:24:24 +0900

    fix test

--- test/lib/OrgModeSyntax/List.php
+++ test/lib/OrgModeSyntax/List.php
@@ -5,8 +5,8 @@ $t = new lime_test;
 
 $p = new OrgModeSyntax_List(PEG::anything());
 $c = PEG::context(array(
-    '- List item 1',
-    '-- List item 1-1',
+    '+ List item 1',
+    '++ List item 1-1',
   ));
 
 $root = $p->parse($c);
@@ -14,8 +14,8 @@ $t->is($root->getType(), 'root');
 
 $nodes = $root->getChildren();
 $t->is($nodes[0]->getType(), 'node');
-$t->is($nodes[0]->getValue(), array('-', str_split(' List item 1')));
+$t->is($nodes[0]->getValue(), array('+', str_split(' List item 1')));
 
 $leaf = $nodes[0]->getChildren();
 $t->is($leaf[0]->getType(), 'leaf');
-$t->is($leaf[0]->getValue(), array('-', str_split(' List item 1-1')));
+$t->is($leaf[0]->getValue(), array('+', str_split(' List item 1-1')));
